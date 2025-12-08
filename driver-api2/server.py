bdfbc7f2acf7a2d3ab43417a59d9e3acd348bf6e
Kenzy1995 <168628850+Kenzy1995@users.noreply.github.com>
2025-12-08 16:30:29 +0800

    Update server.py

--- driver-api2/server.py
+++ driver-api2/server.py
@@ -217,11 +217,7 @@ app = FastAPI(title="Shuttle Driver API", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        # 未來可加上正式前端網域
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
