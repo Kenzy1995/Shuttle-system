4d20ed39f9d4b2165a0e6b3743a186c055a21e11
Kenzy1995 <168628850+Kenzy1995@users.noreply.github.com>
2025-12-23 00:59:14 +0800

    Update server.py

--- booking-api/server.py
+++ booking-api/server.py
@@ -347,6 +347,7 @@ def api_realtime_location():
         return jsonify({"error": str(e)}), 500
 
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
